13e9179f3ad8d3c778d019d3dace008f16911d20
borketh <does.not.kompute@gmail.com>
2024-12-03 23:09:09 -0500

    make more configurable flag options

--- docker/build_targeted_march.sh
+++ docker/build_targeted_march.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-make clean
-./configure --with-extra-cflags="-march=${1} -O3" --with-extra-cxxflags="-march=${1} -O3" --with-boot-jdk="./zulu7.56.0.11-ca-jdk7.0.352-linux_x64" --with-vendor-name="AshleighHost Custom Build" --with-vendor-url="https://homesrv.ashleighhost.net/jdk" --with-vendor-bug-url="amoore@ashleighhost.net"
-make JOBS=14 images
+make dist-clean
+rm ../java-builds/java_8-$1.tar
+./configure --with-extra-cflags="${2}" --with-extra-cxxflags="${2}" --with-boot-jdk="./zulu7.56.0.11-ca-jdk7.0.352-linux_x64" --with-vendor-name="AshleighHost Custom Build" --with-vendor-url="https://homesrv.ashleighhost.net/jdk" --with-vendor-bug-url="amoore@ashleighhost.net" --with-milestone="fcs"
+make JOBS=12 images
 7zz a -ttar ../java-builds/java_8-$1.tar ./build/linux-x86_64-normal-server-release/images/j2sdk-image/*
 make clean
